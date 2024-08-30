8b420b200d799fc1c2735468b2ea6781d9e1f0cc
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-30 18:35:23 -0300

    refac: mudando estrutura de pastas Candidate

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,8 +1,8 @@
 package br.com.gestao_vagas.gestao_vagas.modules.candidate.useCases;
 
 import br.com.gestao_vagas.gestao_vagas.exceptions.UserFoundException;
-import br.com.gestao_vagas.gestao_vagas.modules.candidate.CandidateEntity;
-import br.com.gestao_vagas.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.gestao_vagas.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import br.com.gestao_vagas.gestao_vagas.modules.candidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
