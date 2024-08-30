69db7b944e54ced8c6aad86813ce0928e4d43d97
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-30 18:34:32 -0300

    feat: fluxo de Create Jobs

---
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,26 @@
+package br.com.gestao_vagas.gestao_vagas.modules.company.controllers;
+
+import br.com.gestao_vagas.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gestao_vagas.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+        try {
+            return ResponseEntity.ok().body(this.createJobUseCase.execute(jobEntity));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+}
