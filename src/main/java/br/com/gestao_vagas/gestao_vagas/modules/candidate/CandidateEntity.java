284c3c5246a8b129f5ecbed81e64b63f0b757f68
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-30 15:53:33 -0300

    initial commit

---
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,15 @@
+package br.com.gestao_vagas.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+}
