403e01f7c02605c9d73e8f7c7f6a281ed21e9a68
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-30 17:04:30 -0300

    feat: fluxo de Create candidate

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,12 @@
 package br.com.gestao_vagas.gestao_vagas.modules.candidate.controllers;
 
+import br.com.gestao_vagas.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.gestao_vagas.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping("/")
-    public void create() {
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
     }
 
