3808aedc785d7ae1ba05cbb8e11368190046a835
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-30 21:15:14 -0300

    refac: renomenado Usuario -> Username

--- src/main/java/br/com/gestao_vagas/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/exceptions/UserFoundException.java
@@ -2,6 +2,6 @@ package br.com.gestao_vagas.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException{
     public UserFoundException() {
-        super("Usuário já existe");
+        super("Username já existe");
     }
 }
