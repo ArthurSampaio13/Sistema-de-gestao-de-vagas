5c39b13279be8a65312122833b92d01fd3fa4240
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-08-31 16:39:21 -0300

    fix: trocando Entity por DTO

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,11 +1,18 @@
 package br.com.gestao_vagas.gestao_vagas.modules.company.controllers;
 
+import br.com.gestao_vagas.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gestao_vagas.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gestao_vagas.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -15,8 +22,15 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         try {
+            var companyId = request.getAttribute("company_id");
+            var jobEntity = JobEntity.builder()
+                    .benefits(createJobDTO.getBenefits())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .level(createJobDTO.getLevel())
+                    .description(createJobDTO.getDescription())
+                    .build();
             return ResponseEntity.ok().body(this.createJobUseCase.execute(jobEntity));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
