6ca6f52849b851687d3a8bd0c2a18a49ccefaef9
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-09-01 12:30:38 -0300

    refactor: renomeando as rotas de AuthCandidate

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.naming.AuthenticationException;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/company")
 public class AuthCompanyController {
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
 
-    @PostMapping("/company")
+    @PostMapping("/auth")
     public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         try {
             return ResponseEntity.ok().body(authCompanyUseCase.execute(authCompanyDTO));
