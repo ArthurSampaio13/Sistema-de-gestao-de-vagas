886816e36bf6a942b0a2e8bb2239b9145b3ac521
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-09-02 18:06:22 -0300

    feat: melhorando a documentação swagger

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,10 +1,14 @@
 package br.com.gestao_vagas.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+    @Schema(example = "Vaga para pessoa desenvolvedora Junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "GYMPass, Plano de saude", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+    @Schema(example = "Junior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
