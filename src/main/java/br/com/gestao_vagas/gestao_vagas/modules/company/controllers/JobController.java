1f1c4d8ada0540ead69f3c5660f91df8d42694a9
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-09-02 18:07:09 -0300

    refac: melhorando a documentação swagger

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,14 @@ package br.com.gestao_vagas.gestao_vagas.modules.company.controllers;
 import br.com.gestao_vagas.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.gestao_vagas.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.gestao_vagas.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +32,16 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('company')")
+    @Tag(name = "Job", description = "Informações sobre vagas")
+    @Operation(summary = "Cadastro de vagas",
+            description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class)),
+            }),
+            @ApiResponse(responseCode = "400", description = "User not found")
+    })
+    @SecurityRequirement(name = "JWTAuth")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         try {
             var companyId = request.getAttribute("company_id");
