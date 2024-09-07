cb37dbe2f3493c2de4171f336331af32c9c1706d
Arthur <arthur.sampaio@upe.br>
2024-09-07 20:16:21 -0300

    fix: verificando se a company existe antes de salvar

--- src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/gestao_vagas/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,6 +1,8 @@
 package br.com.gestao_vagas.gestao_vagas.modules.company.useCases;
 
+import br.com.gestao_vagas.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.gestao_vagas.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gestao_vagas.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.gestao_vagas.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,8 +13,15 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository jobRepository;
 
-    public JobEntity execute(JobEntity jobEntity) {
-        return this.jobRepository.save(jobEntity);
+    @Autowired
+    private CompanyRepository companyRepository;
 
+    public JobEntity execute(JobEntity jobEntity) {
+        this.companyRepository.findById(jobEntity.getCompany_id()).orElseThrow(
+                () -> {
+                    throw new CompanyNotFoundException();
+                }
+        );
+        return this.jobRepository.save(jobEntity);
     }
 }
